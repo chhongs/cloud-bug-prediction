afb3be2228e681cb3feeacf2e5eeb06ec44c5060
Anna Jancso <anna.t.jancso@gmail.com>
2019-11-23 14:47:50 +0100

    Improve doc for asat usage extractor

--- ASAT/asat_usage_extractor.py
+++ ASAT/asat_usage_extractor.py
@@ -6,39 +6,45 @@ from model.asat_usage import ASATUsage
 
 
 class ASATUsageExtractor:
-    """Extract ASAT usages from repositories."""
+    """Extract ASAT usages from a repository."""
 
     def __init__(self, asats):
         self.asats = asats
 
     def extract(self, repo_path):
+        """Extract the ASATs used in the given repository."""
         asat_usages = []
 
         for asat in self.asats:
-            asat_cmd_usages = self.get_cmd_usages(repo_path, asat)
-            if asat_cmd_usages:
-                asat_name = asat.name
-                cfg_path = None
-                files = set()
-                args = set()
-
-                for cmd_usage in asat_cmd_usages:
-                    filepath, cmd = cmd_usage.split(':', maxsplit=1)
-                    files.add(Path(filepath).name)
-
-                    argline = re.search(rf'{asat.command}(.*)', cmd).group(1)
-                    for arg in re.finditer(r'--?\S+', argline):
-                        args.add(arg.group())
-
-                asat_usage = ASATUsage(
-                    asat=asat_name, cfg_path=cfg_path, files=files, args=args)
+            asat_usage = self.get_asat_usage(repo_path, asat)
+            if asat_usage:
                 asat_usages.append(asat_usage)
 
         return asat_usages
 
+    @classmethod
+    def get_asat_usage(cls, repo_path, asat):
+        """Create an ASATUsage instance if ASAT is used in the repository."""
+        asat_cmd_usages = cls.get_cmd_usages(repo_path, asat)
+        if asat_cmd_usages:
+            asat_name = asat.name
+            cfg_path = None
+            files = set()
+            args = set()
+
+            for cmd_usage in asat_cmd_usages:
+                filepath, cmd_statement = cmd_usage.split(':', maxsplit=1)
+                files.add(Path(filepath).name)
+                args.update(cls.get_args(cmd_statement, asat.command))
+
+            return ASATUsage(
+                asat=asat_name, cfg_path=cfg_path, files=files, args=args)
+
+        return None
+
     @staticmethod
     def get_cmd_usages(repo_path, asat):
-        """Get command usages for a specific ASAT in the repository's files."""
+        """Get command usages of a specific ASAT in the repository's files."""
         cmd_usages = []
         proc = subprocess.run(
             f'grep -r "{asat.command}[^A-Za-z]" {repo_path}',
@@ -52,3 +58,13 @@ class ASATUsageExtractor:
                     cmd_usages.append(cmd_usage)
 
         return cmd_usages
+
+    @staticmethod
+    def get_args(cmd_statement, asat_cmd):
+        """Get arguments in an command statement."""
+        args = set()
+        argline = re.search(rf'{asat_cmd}(.*)', cmd_statement).group(1)
+        for arg in re.finditer(r'--?\S+', argline):
+            args.add(arg.group())
+
+        return args
