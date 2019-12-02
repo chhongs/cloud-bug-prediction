311692f38a8aa4bb25de8845999ea76420699743
Atif <atifghulamnabi@users.noreply.github.com>
2019-12-02 15:54:52 +0100

    File issue fixed
    -file path set to local repository
    -file duplicate issue fixed

--- DataExtractor/src/main/java/com/mycompany/dataextractor/GenerateCSV.java
+++ DataExtractor/src/main/java/com/mycompany/dataextractor/GenerateCSV.java
@@ -21,16 +21,20 @@ public class GenerateCSV {
 
     public static void main(String[] args) throws IOException, GitAPIException {
         String repoPath = args[0];
+//        String repoPath = "C:/HadoopProject/hadoop/.git";
         repo = new FileRepository(repoPath);
         git = new Git(repo);
         List<Integer> versions = new ArrayList<>();
+        // Test for version 323(submarine-0.2.0-RC0)
         IntStream.range(323, 324).forEach(
                 versions::add
         );
         CommitsData2 commitsData2 = new CommitsData2();
+        // Get file_names_for_commit and total_files_in_commit for all commits in this version
         List<MatrixData> matrixDataList = commitsData2.getCommits(versions, repo, git);
         MatrixComputation matrixComputation = new MatrixComputation();
-        matrixDataList = matrixComputation.computeMatrix(versions, repo, git, matrixDataList);
+        //Compute metrics for the files(for each commit in this release) we get in matrixDataList
+        matrixDataList = matrixComputation.computeMatrix(versions, repo, git, matrixDataList, repoPath);
         if (matrixDataList == null) {
             System.out.println("Version is not correct");
             return;
@@ -52,10 +56,18 @@ public class GenerateCSV {
             List<String[]> data = new ArrayList<>();
             data.add(new String[]{"name-pr", "version", "name", "loc", "wmc", "dit", "cbo", "rfc", "lcom", "bug"});
 
+//            for (MatrixData matrixData : unique) {
+//                data.add(new String[]{matrixData.getNamePr(), matrixData.getVersion(), matrixData.getClassName(),
+//                        matrixData.getLoc(), matrixData.getWmc(), matrixData.getDit(), matrixData.getCbo(),
+//                        matrixData.getRfc(), matrixData.getLcom(),String.valueOf(matrixData.getBug())});
+//            }
             for (MatrixData matrixData : unique) {
-                data.add(new String[]{matrixData.getNamePr(), matrixData.getVersion(), matrixData.getClassName(),
-                        matrixData.getLoc(), matrixData.getWmc(), matrixData.getDit(), matrixData.getCbo(),
-                        matrixData.getRfc(), matrixData.getLcom(),String.valueOf(matrixData.getBug())});
+                // Ignore the files not found
+                if (matrixData.getLoc() != null){
+                    data.add(new String[]{matrixData.getNamePr(), matrixData.getVersion(), matrixData.getClassName(),
+                            matrixData.getLoc(), matrixData.getWmc(), matrixData.getDit(), matrixData.getCbo(),
+                            matrixData.getRfc(), matrixData.getLcom(), String.valueOf(matrixData.getBug())});
+                }
             }
             // create a List which contains String array
             writer.writeAll(data);
