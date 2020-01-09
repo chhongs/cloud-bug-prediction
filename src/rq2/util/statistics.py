d8834520db5ddb804be96d0eb6ad2ceac3b20250
Anna Jancso <anna.t.jancso@gmail.com>
2020-01-09 19:31:04 +0100

    Compute statistics for ASAT categories

--- src/rq2/util/statistics.py
+++ src/rq2/util/statistics.py
@@ -17,6 +17,20 @@ def get_asat_usage_numbers(projects: List[Project]):
     return numbers
 
 
+def get_asat_category_usage_numbers(projects: List[Project]):
+    """Get number of projects per ASAT category."""
+    numbers = {}
+    for project in projects:
+        for asat_usage in project.asat_usages:
+            category = asat_usage.asat.category
+            if category is not None:
+                if category not in numbers:
+                    numbers[category] = set()
+                numbers[category].add(project.url)
+
+    return {c: len(numbers[c]) for c in numbers}
+
+
 def plot_asat_usage_percentages(asats, projects):
     numbers = get_asat_usage_numbers(projects)
     n_projects = len(projects)
@@ -25,21 +39,33 @@ def plot_asat_usage_percentages(asats, projects):
         if asat.name not in numbers:
             numbers[asat.name] = 0
 
+    plot(n_projects, numbers)
+
+
+def plot_asat_category_usage_percentages(asats, projects):
+    numbers = get_asat_category_usage_numbers(projects)
+    n_projects = len(projects)
+
+    categories = {asat.category for asat in asats if asat.category is not None}
+    for category in categories:
+        if category not in numbers:
+            numbers[category] = 0
+
+    plot(n_projects, numbers, (15, 2))
+
+
+def plot(n_projects, numbers, figsize=(15, 15)):
     sorted_numbers = {k: numbers[k] for k in sorted(numbers)}
-
-    group_data = [(num / n_projects)*100 for num in sorted_numbers.values()]
+    group_data = [(num / n_projects) * 100 for num in sorted_numbers.values()]
     group_names = list(sorted_numbers.keys())
-
-    fig, ax = plt.subplots(figsize=(15, 15))
+    fig, ax = plt.subplots(figsize=figsize)
     ax.barh(group_names, group_data)
     ax.set(xlabel='Percentage',
            ylabel='ASAT',
            title='Percentages for ASATs used in projects')
     ax.xaxis.set_ticks(np.arange(0, 110, 10))
-
     for i, v in enumerate(group_data):
         ax.text(v + 1, i - .15, str(round(v, 0)), color='blue')
-
     plt.show()
 
 
@@ -82,6 +108,7 @@ def print_asat_arg_usage(projects: List[Project]):
 def compute_statistics(projects, asats):
     print('Number of projects: ', len(projects))
     plot_asat_usage_percentages(asats, projects)
+    plot_asat_category_usage_percentages(asats, projects)
     print_average_number_of_asats(projects)
     print_asat_arg_usage(projects)
     print()
